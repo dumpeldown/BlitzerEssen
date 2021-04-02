c9399d4c1dc8deb549c6cb799a0d453b6f3a6416
dumpeldown <jurek_miklas.jesse@smail.th-koeln.de>
2021-04-02 22:04:36 +0200

    - read from prop file - add getter for values in prop file - get number of days in image from prop file

--- src/main/java/de/dumpeldown/blitzer/ocr/OCRManager.java
+++ src/main/java/de/dumpeldown/blitzer/ocr/OCRManager.java
@@ -10,60 +10,97 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OCRManager {
     private static Tesseract tesseract;
     private static BufferedImage bufferedImage;
-    public OCRManager(){
+    private static String IMAGE_PROPERTIES;
+
+    private static final String PROP_NAME = Main.IMAGE_NAME.split("\\.")[0]+".prop";
+
+    public OCRManager() {
         tesseract = new Tesseract();
         File f = new File(".");
-        System.out.println(f.getAbsolutePath());
         String absolutPath = f.getAbsolutePath();
-        absolutPath = absolutPath.substring(0,absolutPath.length()-1);
-        tesseract.setDatapath(absolutPath+"src\\main\\resources\\de\\dumpeldown\\blitzer\\ocr" +
+        absolutPath = absolutPath.substring(0, absolutPath.length() - 1);
+        tesseract.setDatapath(absolutPath + "src\\main\\resources\\de\\dumpeldown\\blitzer\\ocr" +
                 "\\tesseract_data");
         tesseract.setLanguage("deu");
     }
 
-    public void init(){
+    public boolean init() {
         try {
             bufferedImage = ImageIO.read(getClass().getResource(Main.IMAGE_NAME));
+            byte[] propByte;
+            System.out.println(PROP_NAME);
+            try {
+                propByte =
+                        getClass().getResource(PROP_NAME).openStream().readAllBytes();
+            }catch(NullPointerException e){
+                System.out.println("Prop Datei für die ausgewählte Datei nicht gefunden.");
+                return false;
+            }
+            System.out.println(
+                Arrays.toString(propByte)
+            );
+            IMAGE_PROPERTIES =
+                    new String(
+                        propByte
+                    );
+            System.out.println("IMAGE_PROPERTIES gelesen: " + IMAGE_PROPERTIES);
         } catch (IOException exception) {
             exception.printStackTrace();
+            return false;
         }
+        return true;
     }
 
     public ArrayList<String> pngToText() {
         ArrayList<String> straßen = new ArrayList<>();
-        int width = bufferedImage.getWidth();
-        int height = bufferedImage.getHeight();
+        double width = bufferedImage.getWidth();
+        double height = bufferedImage.getHeight();
         System.out.println("Gesamt Breite des Bildes: " + width);
         System.out.println("Gesamt Höhe des Bildes: " + height);
-        int xStart = 30;
-        int yStart = 115;
+        double xStart = getXStart();
+        double yStart = getYStart();
+        System.out.println("xStart= "+xStart);
+        System.out.println("yStart= "+yStart);
         //höhe ohne header
         // breite ohne ränder, jede spalte ist gleich breit
         height = height - yStart;
         width -= (xStart * 2);
         System.out.println("Höhe des Bildes ohne 'Header': " + height);
-        for (int spalte = 0; spalte < 7; spalte++) {
+        for (int spalte = 0; spalte < getAmountOfDays(); spalte++) {
             System.out.println("Aktuelle Spalte: " + spalte);
             System.out.println("Trying to read at coordinates: \n" +
                     "x = " + (xStart + ((width / 7) * spalte)) +
                     "\ny = " + yStart +
-                    "\nwidth = " + width / 7 +
+                    "\nwidth = " + width / getAmountOfDays() +
                     "\nheight = " + height);
             try {
                 straßen.add(tesseract.doOCR(bufferedImage,
                         new Rectangle(
-                                xStart + ((width / 7) * spalte),
-                                yStart,
-                                width / 7,
-                                height)));
+                                (int) (xStart + ((width / getAmountOfDays()) * spalte)),
+                                (int) yStart,
+                                (int) (width / getAmountOfDays()),
+                                (int) height)));
             } catch (TesseractException e) {
                 e.printStackTrace();
             }
         }
         return straßen;
+    }
+
+    private double getYStart() {
+        return Double.parseDouble(IMAGE_PROPERTIES.split(";")[1].split("=")[1].trim());
+    }
+
+    private double getXStart() {
+        return Double.parseDouble(IMAGE_PROPERTIES.split(";")[0].split("=")[1].trim());
+    }
+
+    private int getAmountOfDays(){
+        return Integer.parseInt(IMAGE_PROPERTIES.split(";")[2].split("=")[1].trim());
     }
 }
