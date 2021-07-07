38d3547baa28f720283af9807bd4be7f43dff837
dumpeldown <jurek.jesse@gmail.com>
2021-07-07 23:03:24 +0200

    refactor reading image into byte array and misc

--- src/main/java/de/dumpeldown/blitzer/ocr/OCRManager.java
+++ src/main/java/de/dumpeldown/blitzer/ocr/OCRManager.java
@@ -3,6 +3,7 @@ package de.dumpeldown.blitzer.ocr;
 import de.dumpeldown.blitzer.main.Main;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FileUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -10,14 +11,13 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class OCRManager {
     private static Tesseract tesseract;
     private static BufferedImage bufferedImage;
     private static String IMAGE_PROPERTIES;
 
-    private static final String PROP_NAME = Main.IMAGE_NAME.split("\\.")[0]+".prop";
+    private static final String PROP_NAME = Main.IMAGE_NAME.split("\\.")[0] + ".prop";
 
     public OCRManager() {
         tesseract = new Tesseract();
@@ -31,22 +31,17 @@ public class OCRManager {
 
     public boolean init() {
         try {
-            bufferedImage = ImageIO.read(getClass().getResource(Main.IMAGE_NAME));
+            bufferedImage = ImageIO.read(new File("./images/" + Main.IMAGE_NAME));
             byte[] propByte;
-            System.out.println(PROP_NAME);
             try {
-                propByte =
-                        getClass().getResource(PROP_NAME).openStream().readAllBytes();
-            }catch(NullPointerException e){
+                propByte = FileUtils.readFileToByteArray(new File("./images/" + PROP_NAME));
+            } catch (NullPointerException e) {
                 System.out.println("Prop Datei für die ausgewählte Datei nicht gefunden.");
                 return false;
             }
-            System.out.println(
-                Arrays.toString(propByte)
-            );
             IMAGE_PROPERTIES =
                     new String(
-                        propByte
+                            propByte
                     );
             System.out.println("IMAGE_PROPERTIES gelesen: " + IMAGE_PROPERTIES);
         } catch (IOException exception) {
@@ -57,27 +52,33 @@ public class OCRManager {
     }
 
     public ArrayList<String> pngToText() {
+        double xStart = 0, yStart = 0;
         ArrayList<String> straßen = new ArrayList<>();
         double width = bufferedImage.getWidth();
         double height = bufferedImage.getHeight();
         System.out.println("Gesamt Breite des Bildes: " + width);
         System.out.println("Gesamt Höhe des Bildes: " + height);
-        double xStart = getXStart();
-        double yStart = getYStart();
-        System.out.println("xStart= "+xStart);
-        System.out.println("yStart= "+yStart);
+        try {
+            xStart = getXStart();
+            yStart = getYStart();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Malformed or misconfigured .prop file.");
+        }
+        System.out.println("xStart= " + xStart);
+        System.out.println("yStart= " + yStart);
         //höhe ohne header
         // breite ohne ränder, jede spalte ist gleich breit
         height = height - yStart;
         width -= (xStart * 2);
-        System.out.println("Höhe des Bildes ohne 'Header': " + height);
         for (int spalte = 0; spalte < getAmountOfDays(); spalte++) {
-            System.out.println("Aktuelle Spalte: " + spalte);
+            System.out.println("Bearbeite " + spalte + " von " + getAmountOfDays());
+            /*
             System.out.println("Trying to read at coordinates: \n" +
                     "x = " + (xStart + ((width / 7) * spalte)) +
                     "\ny = " + yStart +
                     "\nwidth = " + width / getAmountOfDays() +
                     "\nheight = " + height);
+            */
             try {
                 straßen.add(tesseract.doOCR(bufferedImage,
                         new Rectangle(
@@ -100,7 +101,7 @@ public class OCRManager {
         return Double.parseDouble(IMAGE_PROPERTIES.split(";")[0].split("=")[1].trim());
     }
 
-    private int getAmountOfDays(){
+    private int getAmountOfDays() {
         return Integer.parseInt(IMAGE_PROPERTIES.split(";")[2].split("=")[1].trim());
     }
 }
